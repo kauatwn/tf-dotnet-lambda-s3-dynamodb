cc4bb9a82ac5342491354839ae3bab89891fa839
kauatwn <kaualopescontato@gmail.com>
2026-07-12 18:39:02 -0300

    refactor(infra): update variable descriptions

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,11 +1,11 @@
 variable "lambda_architecture" {
   type        = string
-  description = "A arquitetura da máquina onde a Lambda vai rodar (x86_64 ou arm64)"
+  description = "The architecture of the machine where the Lambda will run (x86_64 or arm64)"
   default     = "x86_64"
 }
 
 variable "localstack_endpoint" {
   type        = string
-  description = "URL do endpoint do LocalStack (ex: http://127.0.0.1:4566)"
+  description = "LocalStack endpoint URL (e.g., http://127.0.0.1:4566)"
   default     = "http://127.0.0.1:4566"
 }
