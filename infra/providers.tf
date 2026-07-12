ee8751f8b4023bfc85f17ed467bda731a39cac62
Kauã Lima <119634784+kauatwn@users.noreply.github.com>
2026-07-12 14:28:11 -0300

    ci: add github actions workflow for localstack integration (#3)

--- infra/providers.tf
+++ infra/providers.tf
@@ -24,11 +24,11 @@ provider "aws" {
   skip_requesting_account_id  = true
 
   endpoints {
-    s3         = "http://localhost:4566"
-    dynamodb   = "http://localhost:4566"
-    apigateway = "http://localhost:4566"
-    lambda     = "http://localhost:4566"
-    iam        = "http://localhost:4566"
-    sts        = "http://localhost:4566"
+    s3         = var.localstack_endpoint
+    dynamodb   = var.localstack_endpoint
+    apigateway = var.localstack_endpoint
+    lambda     = var.localstack_endpoint
+    iam        = var.localstack_endpoint
+    sts        = var.localstack_endpoint
   }
 }
--- infra/variables.tf
+++ infra/variables.tf
@@ -3,3 +3,9 @@ variable "lambda_architecture" {
   description = "A arquitetura da máquina onde a Lambda vai rodar (x86_64 ou arm64)"
   default     = "x86_64"
 }
+
+variable "localstack_endpoint" {
+  type        = string
+  description = "URL do endpoint do LocalStack (ex: http://127.0.0.1:4566)"
+  default     = "http://127.0.0.1:4566"
+}
