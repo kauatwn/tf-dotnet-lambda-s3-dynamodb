2de254346b0d0093c48cef371d144137a2fb8b50
kauatwn <kaualopescontato@gmail.com>
2026-07-17 20:22:19 -0300

    feat: add api_id output for REST API Gateway in outputs.tf

--- infra/environments/dev/outputs.tf
+++ infra/environments/dev/outputs.tf
@@ -2,3 +2,8 @@ output "api_url" {
   value       = module.api.api_url
   description = "Generated local URL to send images (POST)"
 }
+
+output "api_id" {
+  value       = module.api.api_id
+  description = "The ID of the REST API Gateway"
+}
--- infra/modules/api/outputs.tf
+++ infra/modules/api/outputs.tf
@@ -2,3 +2,8 @@ output "api_url" {
   value       = "${aws_api_gateway_stage.api_stage.invoke_url}/${aws_api_gateway_resource.images_resource.path_part}"
   description = "Generated local URL to send images (POST)"
 }
+
+output "api_id" {
+  value       = aws_api_gateway_rest_api.image_api.id
+  description = "The ID of the REST API Gateway"
+}
