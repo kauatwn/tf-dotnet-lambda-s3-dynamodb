02728614ca9cce2e50168e993790e499c691ea5f
kauatwn <kaualopescontato@gmail.com>
2026-07-18 17:50:38 -0300

    refactor: add IMetadataRepository and IStorageService interfaces

--- src/ImageProcessor.Core/Interfaces/IStorageService.cs
+++ src/ImageProcessor.Core/Interfaces/IStorageService.cs
@@ -1,6 +1,6 @@
-namespace ImageProcessor.Lambda.Core;
+namespace ImageProcessor.Core.Interfaces;
 
-public interface IStorage
+public interface IStorageService
 {
     Task<string> UploadBase64ImageAsync(string base64Image, string fileName, string contentType);
 }
